34f4dac0255c0c0ee8cc7bea773a766257c2006d
LiLiParty <okadango33@gmail.com>
2021-05-29 17:46:34 +0900

    add requirements of heroku

--- ComputerVision/ComV_sample.py
+++ ComputerVision/ComV_sample.py
@@ -18,8 +18,8 @@ import streamlit as st
 from streamlit.type_util import is_altair_chart
 
 #st.title("Delete APIKEY")
-KEY = st.secrets['API_KEY']
-ENDPOINT = st.secrets['END_POINT']
+KEY = os.environ['API_KEY']
+ENDPOINT = os.environ['API_ENDPOINT']
 
 computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))
 
@@ -66,10 +66,11 @@ def detect_adult_remote(img_url):
 
 
 st.title("ComputerVision Sample")
+st.write("画像をアップロードするか、URLを入力してください。")
 
 upload_file = st.file_uploader(
-    'Choose an image  !!!!!現在こちらは正常に動きません、下のURLを入力して実行してください!!!!!',
-    type=['png','jpg','jpeg']
+    'Choose an image  現在こちらは正常に動かないことがあります。尚、アップロードした画像はネットワーク上に保存されますが、削除も可能です。',
+    type=['png','jpg','jpeg'],
     )
 
 
@@ -120,7 +121,7 @@ if upload_file is not None:
         if a:
             flag = 1
             for i in range(100):
-                latest_iteration.text(f"Iteration {i+1}")
+                latest_iteration.text(f"load {i+1}")
                 bar.progress(i + 1)
                 time.sleep(0.01)
             "Delete!!"
@@ -133,15 +134,12 @@ if upload_file is not None:
     except:
         st.error("アップロード機能は現環境では動きません！")
 
-img_url = st.text_input('Enter some URL of image',value="https;//example_of_img.jpg")
+img_url = st.text_input('Enter some URL of image',value="https://images.unsplash.com/photo-1621570168297-bdcdd4457664?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80")
 
 if img_url is not None:
-        img_read = urllib.request.urlopen(img_url).read() #画像データGET
-        img_bin = io.BytesIO(img_read) #メモリに保持してディレクトリ偽装みたいなことする
-        pil_img = Image.open(img_bin) #PILで読み込む
-        img_io = io.BytesIO() #空のインスタンスを作る
-        pil_img.save(img_io,"JPEG") #空のインスタンスに保存する
-        img = img_io.getvalue() #バイナリデータを取得する（open().read()状態）
+    try:
+        f = io.BytesIO(urllib.request.urlopen(img_url).read())
+        img = Image.open(f)
 
 
         if detect_adult_remote(img_url):
@@ -160,7 +158,7 @@ if img_url is not None:
             caption = object.object_property
 
             font = ImageFont.truetype(font='./Helvetica 400.ttf', size=30)
-            text_w,text_h = draw.textsize(caption, font=font)
+            text_w,text_h = draw.textsize(caption,font=font)
             
 
             draw.rectangle([(x,y),(x+w, y+h)], fill=None, outline='green', width=8)
@@ -174,5 +172,7 @@ if img_url is not None:
         tags_name = ', '.join(tags_name)
         st.markdown('*detected contents tag*')
         st.markdown(f'>{tags_name}')
+    except:
+        st.error("正しいURLを入力してください！")
 
-
+st.write("made by LiLiParty")
